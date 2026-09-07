4841f586d40bec72a96ae052c8a1b59f8173f361
TechnoWizardX <artyom.lagutskiy@gmail.com>
2026-09-07 21:30:08 +0300

    fix: not visalize code editor and directory view. qss: frame borded 1 px -> 2px

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,7 +1,10 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QSplitter, QVBoxLayout
+from PySide6.QtCore import Qt
 import sys
 from src.utils.config_manager import ConfigManager
 from src.utils.theme_manager import ThemeManager
+from src.gui.directory_view import DirectoryTreeViewWidget
+from src.gui.code_editor import CodeEditorWidget
 
 
 class MainWindow(QMainWindow):
@@ -9,6 +12,22 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.theme_manager = theme_manager
         self.setWindowTitle("KB Studio")
+
+        self.main_widget = QWidget()
+        self.setCentralWidget(self.main_widget)
+
+        self.main_layout = QVBoxLayout()
+        self.main_widget.setLayout(self.main_layout)
+
+        self.dir_view = DirectoryTreeViewWidget()
+        self.code_editor = CodeEditorWidget()
+
+        self.main_splitter = QSplitter(Qt.Horizontal)
+        self.main_layout.addWidget(self.main_splitter)
+        
+        self.main_splitter.addWidget(self.dir_view)
+        self.main_splitter.addWidget(self.code_editor)
+        self.main_splitter.setSizes([300, 700])
 
 
 def init_app():
